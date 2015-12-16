6e935ef8710f17c32f055b71bdd9234e20aaed39
Nimrod Lahav <nimrodl@wix.com>
2015-12-16 21:27:02 +0200

    test server

---
+++ scripts/testWazeAppServer.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+APP1="localhost:9090"
+
+clear
+
+echo "app1: $APP1  - Routes Directions"
+response=$(curl --write-out %{http_code} --output /dev/null --silent --get --include "http://$APP1/waze/routesWithDirections?end=156+5th+Avenue%2C+New+York%2C+NY+10010&start=6+East+57th+Street%2C+New+York%2C+NY+10022" -H "Accept: application/json")
+if [ $response != "200" ]; then
+    echo "failed with $response"
+    exit 1
+fi
+echo "passed"
+echo ""
+
+echo "app1: $APP1  - Address List"
+response=$(curl --write-out %{http_code} --output /dev/null --silent --get --include "http://$APP1/waze/addressList?address=6+E+57th+St+New+York" -H "Accept: application/json")
+if [ $response != "200" ]; then
+    echo "failed with $response"
+    exit 1
+fi
+echo "passed"
+echo ""
+
+echo "app1: $APP1  - Routes Calculation"
+response=$(curl --write-out %{http_code} --output /dev/null --silent --get --include "http://$APP1/waze/routes?end=156+5th+Avenue%2C+New+York%2C+NY+10010&start=6+East+57th+Street%2C+New+York%2C+NY+10022" -H "Accept: application/json")
+if [ $response != "200" ]; then
+    echo "failed with $response"
+    exit 1
+fi
+echo "passed"
+echo ""
+
+
+echo "app1: $APP1  - Routes Calculation Coordinates"
+response=$(curl --write-out %{http_code} --output /dev/null --silent --get --include "http://$APP1/waze/routesXY?endLat=40.762966999999996&endLon=-73.973968&startLat=40.76267793274429&startLon=-73.97328385997065" -H "Accept: application/json")
+if [ $response != "200" ]; then
+    echo "failed with $response"
+    exit 1
+fi
+echo "passed"
+echo ""
+
+
+echo "app1: $APP1  - Routes Directions Coordinates"
+response=$(curl --write-out %{http_code} --output /dev/null --silent --get --include "http://$APP1/waze/routesXYwithDirections?endLat=40.762966999999996&endLon=-73.973968&startLat=40.76267793274429&startLon=-73.97328385997065" -H "Accept: application/json")
+if [ $response != "200" ]; then
+    echo "failed with $response"
+    exit 1
+fi
+echo "passed"
+echo ""
+
+
+echo "app1: $APP1  - Traffic Notifications"
+response=$(curl --write-out %{http_code} --output /dev/null --silent --get --include "http://$APP1/waze/traffic-notifications?latBottom=40.72892879374242&latTop=40.733897887617324&lonLeft=-74.0093457698822&lonRight=-73.98462653160095" -H "Accept: application/json")
+if [ $response != "200" ]; then
+    echo "failed with $response"
+    exit 1
+fi
+echo "passed"
+echo ""
+
+echo "Finished"
+
+
